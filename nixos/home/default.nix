4e9eebcc415f467460733835ecc374a83dd3da1b
Ivan <92064752+UltraToon@users.noreply.github.com>
2024-04-06 12:36:56 -0700

    Update default.nix

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -14,12 +14,12 @@
  };
  programs.home-manager.enable = true;
  imports = [
-   ./programs/hypr
+   /* ./programs/hypr
    ./programs/foot
    ./programs/fonts
    ./programs/firefox
    ./programs/mako
    ./programs/git
-   ./programs/nvim
+   ./programs/nvim */
  ];
 }
